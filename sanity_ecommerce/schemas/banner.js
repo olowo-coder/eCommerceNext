c91cf2b1c56e64410395feaa69219016143c266c
olowo-coder <olowofela.olanrewaju@decagon.dev>
2022-05-01 23:00:50 +0100

    products

--- sanity_ecommerce/schemas/banner.js
+++ sanity_ecommerce/schemas/banner.js
@@ -45,7 +45,17 @@ export default{
             name: "largeText2",
             title: "LargeText2",
             type: "string",
-        }
+        },
+        {
+            name: 'discount',
+            title: 'Discount',
+            type: 'string',
+        },
+        {
+            name: 'saleTime',
+            title: 'SaleTime',
+            type: 'string',
+        },
     ]
 
 }
