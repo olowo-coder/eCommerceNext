97adfdaa5691502c1eb8f1318df0aac314a1225e
olowo-coder <olowofela.olanrewaju@decagon.dev>
2022-05-04 22:50:48 +0100

    checkout Dev

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -20,7 +20,7 @@ const Cart = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({cartItems: cartItems}),
+        body: JSON.stringify(cartItems),
       });
 
       if (response.statusCode === 500) return;
--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,4 +1,8 @@
-const stripe = require("stripe")(`sk_test_51KvIcyF3fyBL5xXa8frO71Jxzu7oA4QPBmwVHNEe3eGGGnnvSIjo90E8ACBZdrzBCBerc66OAuykcEc6gslYEB9m00As5Yj8pi`);
+import Stripe from "stripe";
+
+const stripe = new Stripe(`${process.env.STRIPE_SECRET_KEY}`);
+
+// const stripe = require("stripe")(`sk_test_51KvIcyF3fyBL5xXa8frO71Jxzu7oA4QPBmwVHNEe3eGGGnnvSIjo90E8ACBZdrzBCBerc66OAuykcEc6gslYEB9m00As5Yj8pi`);
 
 export default async function handler(req, res) {
     
@@ -9,25 +13,39 @@ export default async function handler(req, res) {
       const params = {
         submit_type: "pay",
         payment_method_types: ["card"],
-        billing_address_collections: "auto",
+        billing_address_collection: "auto",
         shipping_options: [
             { shipping_rate: "shr_1KvJI0F3fyBL5xXamMgjxWi8" },
             { shipping_rate: "shr_1KvJLoF3fyBL5xXabBLfYebd" }
         ],
-        line_items: [
-          {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: "{{PRICE_ID}}",
-            quantity: 1,
-          },
-        ],
+        line_items: req.body.map((item) => {
+            const img = item.image[0].asset._ref;
+            const newImage = img.replace("image", "https://cdn.sanity.io/images/19hrkhwg/production/").replace("-webp", ".webp");
+            console.log("IMAGE", newImage);
+
+            return{
+                price_data: {
+                    currency: "usd",
+                    product_data: {
+                        name: item.name,
+                        images: [newImage],
+                    },
+                    unit_amount: item.price * 100,
+                },
+                adjustable_quantity: {
+                    enabled: true,
+                    minimum: 1
+                },
+                quantity: item.quantity
+            }
+        }),
         mode: "payment",
         success_url: `${req.headers.origin}/?success=true`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
       };
 
       const session = await stripe.checkout.sessions.create(params);
-      res.redirect(303, session.url);
+      res.status(200).json(session);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
